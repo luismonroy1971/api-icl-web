f5d5b8c769684ea980e396d6a414e3071a84ee59
Andrea <andrea@solucionesm4g.com>
2023-11-15 14:56:32 -0500

    fix update file convenio

--- src/routes/convenio.routes.js
+++ src/routes/convenio.routes.js
@@ -1,28 +1,27 @@
 import { Router } from 'express';
 import multer from 'multer'; 
-import { autorizarConvenio, crearConvenio, actualizarConvenio, eliminarConvenio, obtenerPeriodos, leerConvenio, buscarConvenios, activarConvenio, desactivarConvenio } from '../controllers/convenios.controller.js';
-
+import { autorizarConvenio, crearConvenio,actualizarConvenio,eliminarConvenio,obtenerPeriodos, leerConvenio, buscarConvenios, activarConvenio, desactivarConvenio } from '../controllers/convenios.controller.js'
 const router = Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'documentos/convenios/');
+    cb(null, 'documentos/convenios/'); // Ruta donde se guardarán los archivos
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname); // Usa el nombre original del archivo
   },
 });
 
 const upload = multer({ storage: storage });
 
-router.get('/convenios', buscarConvenios);
-router.post('/convenios', upload.fields([{ name: 'pdfFile', maxCount: 10 }]), crearConvenio);
-router.put('/convenios/:id', upload.fields([{ name: 'pdfFile', maxCount: 10 }]), actualizarConvenio);
-router.delete('/convenios/:id', eliminarConvenio);
+// router.get('/convenios',leerConvenios);
+router.get('/convenios',buscarConvenios);
+router.post('/convenios', upload.single('pdfFile'),  crearConvenio);
+router.put('/convenios/:id', upload.single('pdfFile'), actualizarConvenio);
+router.delete('/convenios/:id',eliminarConvenio);
 router.get('/convenios/:id', leerConvenio);
 router.get('/conveniosperiodo', obtenerPeriodos);
-router.put('/autorizarconvenios/:id', autorizarConvenio);
-router.put('/activarconvenios/:id', activarConvenio);
-router.put('/desactivarconvenios/:id', desactivarConvenio);
-
-export default router;
+router.put('/autorizarconvenios/:id',autorizarConvenio);
+router.put('/activarconvenios/:id',activarConvenio);
+router.put('/desactivarconvenios/:id',desactivarConvenio);
+export default router
