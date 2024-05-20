35d0a89c860c3eb423c869bde56400eb52e910c8
luismonroy1971 <34591655+luismonroy1971@users.noreply.github.com>
2024-05-19 22:35:00 -0500

    aumentar a 12 horas el token de acceso

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -1,26 +1,50 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import { Usuario } from '../models/Usuario.js';
-import { OpcionesUsuario } from '../models/OpcionesUsuario.js';
-import { Menu } from '../models/Menu.js';
-import { Sequelize } from 'sequelize';
+import {
+  Usuario
+} from '../models/Usuario.js';
+import {
+  OpcionesUsuario
+} from '../models/OpcionesUsuario.js';
+import {
+  Menu
+} from '../models/Menu.js';
+import {
+  Sequelize
+} from 'sequelize';
 
 export const registrarUsuario = async (req, res) => {
   const t = await Usuario.sequelize.transaction();
 
   try {
-    const { name, email, password, profile, opcionesusuarios } = req.body;
+    const {
+      name,
+      email,
+      password,
+      profile,
+      opcionesusuarios
+    } = req.body;
 
-    const usuarioExistente = await Usuario.findOne({ where: { email } });
+    const usuarioExistente = await Usuario.findOne({
+      where: {
+        email
+      }
+    });
     if (usuarioExistente) {
-      return res.status(400).json({ message: 'El usuario ya existe' });
+      return res.status(400).json({
+        message: 'El usuario ya existe'
+      });
     }
 
     // Registrar el usuario
-    const nuevoUsuario = await Usuario.create(
-      { name, email, password, profile },
-      { transaction: t }
-    );
+    const nuevoUsuario = await Usuario.create({
+      name,
+      email,
+      password,
+      profile
+    }, {
+      transaction: t
+    });
 
     if (opcionesusuarios && opcionesusuarios.length > 0) {
       // Crear instancias de OpcionesUsuario relacionadas con el nuevo usuario
@@ -28,86 +52,116 @@ export const registrarUsuario = async (req, res) => {
         opcionesusuarios.map((opcion) => ({
           id_usuario: nuevoUsuario.id,
           ...opcion,
-        })),
-        { transaction: t }
+        })), {
+          transaction: t
+        }
       );
     } else {
       // Si no se proporcionan opcionesusuarios, revertir la transacción y devolver un mensaje de error
       await t.rollback();
       return res
         .status(400)
-        .json({ message: 'Debe proporcionar al menos una opción de usuario' });
+        .json({
+          message: 'Debe proporcionar al menos una opción de usuario'
+        });
     }
 
     // Confirmar la transacción
     await t.commit();
 
-    res.status(201).json({ message: 'Usuario registrado con éxito' });
+    res.status(201).json({
+      message: 'Usuario registrado con éxito'
+    });
   } catch (error) {
     // Revertir la transacción en caso de error
     await t.rollback();
 
     console.error('Error al registrar usuario:', error);
-    res.status(500).json({ message: 'Error en el servidor' });
+    res.status(500).json({
+      message: 'Error en el servidor'
+    });
   }
 };
 
 // Función para iniciar sesión y generar un token JWT
 export const iniciarSesion = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const {
+      email,
+      password
+    } = req.body;
 
-    const usuario = await Usuario.findOne({ where: { email } });
+    const usuario = await Usuario.findOne({
+      where: {
+        email
+      }
+    });
 
     if (!usuario) {
-      return res.status(401).json({ message: 'Usuario no existe' });
+      return res.status(401).json({
+        message: 'Usuario no existe'
+      });
     }
 
     // Utiliza el método comparePassword para verificar la contraseña
     const contrasenaValida = usuario.comparePassword(password);
 
     if (!contrasenaValida) {
-      return res.status(401).json({ message: 'Clave incorrecta' });
+      return res.status(401).json({
+        message: 'Clave incorrecta'
+      });
     }
 
     const token = generarTokenJWT(usuario);
 
-    res.status(200).json({ token, usuario });
+    res.status(200).json({
+      token,
+      usuario
+    });
   } catch (error) {
     console.error('Error al iniciar sesión:', error);
-    res.status(500).json({ message: 'Error en el servidor', error });
+    res.status(500).json({
+      message: 'Error en el servidor',
+      error
+    });
   }
 };
 
 export const obtenerUsuarios = async (req, res) => {
   try {
     const usuarios = await Usuario.findAll({
-      attributes: { exclude: ['password'] },
-      include: [
-        {
-          model: OpcionesUsuario,
-          attributes: ['id_menu'],
-          include: {
-            model: Menu,
-            attributes: ['nombre_menu'],
-          },
+      attributes: {
+        exclude: ['password']
+      },
+      include: [{
+        model: OpcionesUsuario,
+        attributes: ['id_menu'],
+        include: {
+          model: Menu,
+          attributes: ['nombre_menu'],
         },
-      ],
+      }, ],
     });
 
     const usuariosConOpcionesFormato = usuarios.map((usuario) => {
-      const opcionesFormato = usuario.opcionesusuarios
-        ? usuario.opcionesusuarios
-            .map((opcion) => {
-              return opcion.menu
-                ? { value: opcion.id_menu, label: opcion.menu.nombre_menu }
-                : null;
-            })
-            .filter((opcion) => opcion !== null) // Filter out null values
-        : [];
+      const opcionesFormato = usuario.opcionesusuarios ?
+        usuario.opcionesusuarios
+        .map((opcion) => {
+          return opcion.menu ?
+            {
+              value: opcion.id_menu,
+              label: opcion.menu.nombre_menu
+            } :
+            null;
+        })
+        .filter((opcion) => opcion !== null) // Filter out null values
+        :
+        [];
 
       return {
-        ...usuario.get({ plain: true }),
+        ...usuario.get({
+          plain: true
+        }),
         opcionesusuarios: opcionesFormato,
       };
     });
@@ -115,7 +169,9 @@ export const obtenerUsuarios = async (req, res) => {
     res.status(200).json(usuariosConOpcionesFormato);
   } catch (error) {
     console.error('Error al obtener usuarios:', error);
-    res.status(500).json({ message: 'Error en el servidor' });
+    res.status(500).json({
+      message: 'Error en el servidor'
+    });
   }
 };
 
@@ -125,23 +181,19 @@ export const obtenerUsuarioPorId = async (req, res) => {
     const usuarioId = req.params.id;
 
     const usuario = await Usuario.findByPk(usuarioId, {
-      include: [
-        {
-          model: OpcionesUsuario,
-          include: [
-            {
-              model: Menu,
-              attributes: [
-                'nombre_menu',
-                'etiqueta_menu',
-                'descripcion_menu',
-                'tipo_menu',
-                'url',
-              ],
-            },
+      include: [{
+        model: OpcionesUsuario,
+        include: [{
+          model: Menu,
+          attributes: [
+            'nombre_menu',
+            'etiqueta_menu',
+            'descripcion_menu',
+            'tipo_menu',
+            'url',
           ],
-        },
-      ],
+        }, ],
+      }, ],
       attributes: {
         exclude: ['password'],
         include: [
@@ -151,32 +203,46 @@ export const obtenerUsuarioPorId = async (req, res) => {
     });
 
     if (!usuario) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
+      return res.status(404).json({
+        message: 'Usuario no encontrado'
+      });
     }
 
     // Convertir 'profileObj' de string a objeto JSON
     const usuarioConProfile = {
-      ...usuario.get({ plain: true }),
+      ...usuario.get({
+        plain: true
+      }),
       profile: JSON.parse(usuario.getDataValue('profileObj')),
     };
 
     res.status(200).json(usuarioConProfile);
   } catch (error) {
     console.error('Error al obtener usuario por ID:', error);
-    res.status(500).json({ message: 'Error en el servidor' });
+    res.status(500).json({
+      message: 'Error en el servidor'
+    });
   }
 };
 
 export const actualizarUsuario = async (req, res) => {
   try {
     const usuarioId = req.params.id; // ID del usuario a actualizar
-    const { name, profile, email, password, opcionesusuarios } = req.body; // Nuevos datos del usuario y opciones
+    const {
+      name,
+      profile,
+      email,
+      password,
+      opcionesusuarios
+    } = req.body; // Nuevos datos del usuario y opciones
 
     // Busca el usuario en la base de datos por su ID
     const usuario = await Usuario.findByPk(usuarioId);
 
     if (!usuario) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
+      return res.status(404).json({
+        message: 'Usuario no encontrado'
+      });
     }
 
     // Actualiza los datos del usuario
@@ -196,7 +262,11 @@ export const actualizarUsuario = async (req, res) => {
     // Actualiza las opciones de usuario si se proporcionan en la solicitud
     if (opcionesusuarios && opcionesusuarios.length > 0) {
       // Elimina las opciones actuales del usuario
-      await OpcionesUsuario.destroy({ where: { id_usuario: usuario.id } });
+      await OpcionesUsuario.destroy({
+        where: {
+          id_usuario: usuario.id
+        }
+      });
 
       // Crea instancias de OpcionesUsuario relacionadas con el usuario
       await OpcionesUsuario.bulkCreate(
@@ -207,10 +277,14 @@ export const actualizarUsuario = async (req, res) => {
       );
     }
 
-    res.status(200).json({ message: 'Usuario actualizado correctamente' });
+    res.status(200).json({
+      message: 'Usuario actualizado correctamente'
+    });
   } catch (error) {
     console.error('Error al actualizar usuario:', error);
-    res.status(500).json({ message: 'Error en el servidor' });
+    res.status(500).json({
+      message: 'Error en el servidor'
+    });
   }
 };
 
@@ -223,45 +297,63 @@ const generarTokenJWT = (usuario) => {
 
   const secretKey = 'Icl2023@vamos'; // Reemplaza con tu propia clave secreta
 
-  const token = jwt.sign(payload, secretKey, { expiresIn: '5h' });
+  const token = jwt.sign(payload, secretKey, {
+    expiresIn: '12h'
+  });
 
   return token;
 };
 
 export const activarUsuario = async (req, res) => {
   try {
-    const { id } = req.params;
+    const {
+      id
+    } = req.params;
 
     const usuario = await Usuario.findByPk(id);
 
     if (!usuario) {
-      return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+      return res.status(404).json({
+        mensaje: 'Usuario no encontrado'
+      });
     }
 
     usuario.activo = '1'; // Establecer activo en '1'
     await usuario.save();
 
-    res.json({ mensaje: 'Usuario activado correctamente' });
+    res.json({
+      mensaje: 'Usuario activado correctamente'
+    });
   } catch (error) {
-    return res.status(500).json({ mensaje: error.message });
+    return res.status(500).json({
+      mensaje: error.message
+    });
   }
 };
 
 export const desactivarUsuario = async (req, res) => {
   try {
-    const { id } = req.params;
+    const {
+      id
+    } = req.params;
 
     const usuario = await Usuario.findByPk(id);
 
     if (!usuario) {
-      return res.status(404).json({ mensaje: 'Usuario no encontrado' });
+      return res.status(404).json({
+        mensaje: 'Usuario no encontrado'
+      });
     }
 
     usuario.activo = '0'; // Establecer activo en '0'
     await usuario.save();
 
-    res.json({ mensaje: 'Usuario desactivado correctamente' });
+    res.json({
+      mensaje: 'Usuario desactivado correctamente'
+    });
   } catch (error) {
-    return res.status(500).json({ mensaje: error.message });
+    return res.status(500).json({
+      mensaje: error.message
+    });
   }
 };
